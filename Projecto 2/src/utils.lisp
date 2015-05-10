f43ea626b70562830a0cb7359e6e8f4f63f6d7e8
Diogo Andrade <diogo@yubo.be>
2015-05-11 00:35:48 +0100

    reshape function

---
+++ Projecto 2/src/utils.lisp
@@ -0,0 +1,45 @@
+(defparameter *current-value* -1)
+
+(defun current-value (seq)
+  (if (eq *current-value* (- (length seq) 1))
+     (setf *current-value* 0)
+    (setf *current-value* (+ *current-value* 1)))
+  
+  (setf result (nth *current-value* seq)))
+
+
+(defun fillmatrix (lst seq)
+  (let ((first-element (car lst)))
+    (cond ((endp lst) ())
+          ((numberp first-element) (cons (current-value seq) (fillmatrix (rest lst) seq)))
+          ((listp first-element) (cons (fillmatrix first-element seq) (fillmatrix (rest lst) seq))))))
+
+(defun new-dimension (dim v seq)
+  (loop for x from 1 to dim 
+     collect (if (eq x 1) 
+                  v
+                  (fillmatrix v seq))))
+
+(defun without-last(lst)
+  (cond ((null lst) ())
+        ((null (cdr lst)) ())
+        (t (cons (car lst) (without-last (cdr lst))))))
+
+(defun make-tree-recursive (shape v seq)
+  (let* ((last-el (nth (- (length v) 1) v))
+         (new-dim ()))
+   
+    (if (not (endp shape))
+      (setf new-dim (new-dimension (nth (- (length shape) 1) shape)
+                         last-el
+                         seq)))
+
+    (if (endp shape)
+        ()
+    (cons new-dim
+          (make-tree-recursive (without-last shape)
+                                   (list new-dim)
+                                   seq)))))
+
+(defun make-tree (shape v seq)
+  (cons v (make-tree-recursive shape (list v) seq)))
