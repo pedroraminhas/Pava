cd7c264a656e51c70cbbfcf6cc46510a33b5da8e
Pedro Raminhas <pedro.raminhas@gmail.com>
2015-05-11 23:37:01 +0100

    .* feito

--- Projecto 2/src/dyadicFunctions.lisp
+++ Projecto 2/src/dyadicFunctions.lisp
@@ -71,6 +71,39 @@
 		  (t2-shape (slot-value (shape t2) 'elements)))
 			(if (equal t1-shape t2-shape) (make-instance 'vector-tensor :tensor-elements (addition-tensor-tensor t1-elements t2-elements)) (error "Error: Tensors dont have the same size"))))
 	
+;Multiplication
+(defgeneric .* (x y))
+
+(defmethod .* ((x tensor) (y scalar))
+			 (make-instance 'tensor :tensor-elements (multiplication-tensor-scalar x y))
+	)
+
+(defmethod .* ((x vector-tensor) (y scalar))
+			 (make-instance 'vector-tensor :tensor-elements (multiplication-tensor-scalar x y))
+	)
+
+(defmethod .* ( (y scalar) (x tensor))
+			(make-instance 'tensor :tensor-elements (multiplication-tensor-scalar y x))
+	)
+
+(defmethod .* ( (y scalar) (x vector-tensor))
+			(make-instance 'vector-tensor :tensor-elements (multiplication-tensor-scalar y x))
+	)
+
+
+(defmethod .* ((t1 tensor) (t2 tensor))
+	(let* ((t1-elements (slot-value t1 'elements))
+		  (t2-elements (slot-value t2 'elements))
+		  (t1-shape (slot-value (shape t1) 'elements))
+		  (t2-shape (slot-value (shape t2) 'elements)))
+			(if (equal t1-shape t2-shape) (make-instance 'tensor :tensor-elements (multiplication-tensor-tensor t1-elements t2-elements)) (error "Error: Tensors dont have the same size"))))
+
+(defmethod .* ((t1 vector-tensor) (t2 vector-tensor))
+	(let* ((t1-elements (slot-value t1 'elements))
+		  (t2-elements (slot-value t2 'elements))
+		  (t1-shape (slot-value (shape t1) 'elements))
+		  (t2-shape (slot-value (shape t2) 'elements)))
+			(if (equal t1-shape t2-shape) (make-instance 'vector-tensor :tensor-elements (multiplication-tensor-tensor t1-elements t2-elements)) (error "Error: Tensors dont have the same size"))))
 
 
 
--- Projecto 2/src/functions.lisp
+++ Projecto 2/src/functions.lisp
@@ -16,3 +16,93 @@
 (defun negation (x)
 	(cond ((eq x 0) 1)
 			(t 0)))
+
+(defgeneric subtraction-tensor-scalar (t1 t2))
+
+(defmethod subtraction-tensor-scalar ((t1 tensor) (t2 scalar))
+  (let ((result ())
+     (tensor-elements (slot-value t1 'elements)))
+    (cond ((not (listp (car tensor-elements))) (dolist (it tensor-elements result) (setf result (append result (list (- it (slot-value t2 'elements)))))))
+        (t (loop for x in tensor-elements
+          do (setf result (append result (list (subtraction-tensor-scalar (make-instance 'tensor :tensor-elements x) t2)))))))
+    result))
+
+(defmethod subtraction-tensor-scalar ((t2 scalar) (t1 tensor))
+  (let ((result ())
+     (tensor-elements (slot-value t1 'elements)))
+    (cond ((not (listp (car tensor-elements))) (dolist (it tensor-elements result) (setf result (append result (list (- (slot-value t2 'elements) it))))))
+        (t (loop for x in tensor-elements
+          do (setf result (append result (list (subtraction-tensor-scalar t2 (make-instance 'tensor :tensor-elements x))))))))
+    result))
+
+
+(defun subtraction-tensor-tensor (t1 t2)
+  (let ((result ()))
+    (cond ((not (listp (car t1))) (loop for x in t1
+                      for y in t2
+                     do (setf result (append result (list (- x y))))))
+        (t (loop for x in t1
+             for y in t2
+          do (setf result (append result (list (subtraction-tensor-tensor x y)))))))
+    result))
+
+
+(defgeneric addition-tensor-scalar (t1 t2))
+
+(defmethod addition-tensor-scalar ((t1 tensor) (t2 scalar))
+  (let ((result ())
+     (tensor-elements (slot-value t1 'elements)))
+    (cond ((not (listp (car tensor-elements))) (dolist (it tensor-elements result) (setf result (append result (list (+ it (slot-value t2 'elements)))))))
+        (t (loop for x in tensor-elements
+          do (setf result (append result (list (addition-tensor-scalar (make-instance 'tensor :tensor-elements x) t2)))))))
+    result))
+
+(defmethod addition-tensor-scalar ((t2 scalar) (t1 tensor))
+  (let ((result ())
+     (tensor-elements (slot-value t1 'elements)))
+    (cond ((not (listp (car tensor-elements))) (dolist (it tensor-elements result) (setf result (append result (list (+ (slot-value t2 'elements) it))))))
+        (t (loop for x in tensor-elements
+          do (setf result (append result (list (addition-tensor-scalar t2 (make-instance 'tensor :tensor-elements x))))))))
+    result))
+
+
+(defun addition-tensor-tensor (t1 t2)
+  (let ((result ()))
+    (cond ((not (listp (car t1))) (loop for x in t1
+                      for y in t2
+                     do (setf result (append result (list (+ x y))))))
+        (t (loop for x in t1
+             for y in t2
+          do (setf result (append result (list (addition-tensor-tensor x y)))))))
+    result))
+
+;Multiplication
+
+(defgeneric multiplication-tensor-scalar (t1 t2))
+
+(defmethod multiplication-tensor-scalar ((t1 tensor) (t2 scalar))
+  (let ((result ())
+     (tensor-elements (slot-value t1 'elements)))
+    (cond ((not (listp (car tensor-elements))) (dolist (it tensor-elements result) (setf result (append result (list (* it (slot-value t2 'elements)))))))
+        (t (loop for x in tensor-elements
+          do (setf result (append result (list (multiplication-tensor-scalar (make-instance 'tensor :tensor-elements x) t2)))))))
+    result))
+
+(defmethod multiplication-tensor-scalar ((t2 scalar) (t1 tensor))
+  (let ((result ())
+     (tensor-elements (slot-value t1 'elements)))
+    (cond ((not (listp (car tensor-elements))) (dolist (it tensor-elements result) (setf result (append result (list (* (slot-value t2 'elements) it))))))
+        (t (loop for x in tensor-elements
+          do (setf result (append result (list (multiplication-tensor-scalar t2 (make-instance 'tensor :tensor-elements x))))))))
+    result))
+
+
+(defun multiplication-tensor-tensor (t1 t2)
+  (let ((result ()))
+    (cond ((not (listp (car t1))) (loop for x in t1
+                      for y in t2
+                     do (setf result (append result (list (* x y))))))
+        (t (loop for x in t1
+             for y in t2
+          do (setf result (append result (list (multiplication-tensor-tensor x y)))))))
+    result))
