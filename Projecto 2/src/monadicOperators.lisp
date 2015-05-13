3a8e7aa1c27353d0472d8b29ea0fa2def075a93c
Diogo Andrade <diogo@yubo.be>
2015-05-13 06:35:49 +0100

    outer-product operator

--- Projecto 2/src/monadicOperators.lisp
+++ Projecto 2/src/monadicOperators.lisp
@@ -22,3 +22,27 @@
                                                         (s (nth (- (length result) 1) result)))
                                             'elements)))))
           (make-instance 'tensor :tensor-elements (list result)))))
+
+(defun outer-product (op)
+  #'(lambda (lst tensor-cpy)
+      (let ((t1 (slot-value lst 'elements))
+            (t2 (slot-value tensor-cpy 'elements)))
+      (defun fillt (lst tensor-cpy)
+        (let ((first-element (car lst)))
+          (cond ((endp lst) ()) 
+                ((numberp first-element)      
+                  (cons (funcall (fill-with-value op) tensor-cpy first-element) (fillt (rest lst) tensor-cpy)))
+                ((listp first-element) (cons (fillt first-element tensor-cpy) (fillt (rest lst) tensor-cpy)))))) 
+     
+       (make-instance 'tensor :tensor-elements (fillt t1 t2)))))
+
+; função auxiliar no calculo de cada combinação
+(defun fill-with-value (op)
+  #'(lambda (lst value)
+      (defun fillw (lst value)
+        (let ((first-element (car lst)))
+          (cond ((endp lst) ())
+                ((numberp first-element)                     
+                  (cons (funcall op (s value) (s first-element)) (fillw (rest lst) value)))
+                ((listp first-element) (cons (fillw first-element value) (fillw (rest lst) value)))))) (fillw lst value)))
+
